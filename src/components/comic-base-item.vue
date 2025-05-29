b385d8bcbca1c129ae3af8e4060e5a23fff5278d
Par9uet <1607611087@qq.com>
2025-05-29 17:22:26 +0800

    refactor: 作者过多时进行横向滚动，保持 UI 高度一致

--- src/components/comic-base-item.vue
+++ src/components/comic-base-item.vue
@@ -4,6 +4,70 @@ import { Comic } from "@/apis";
 defineProps<{
   comic: Comic;
 }>();
+
+const authorListContainerElRef = useTemplateRef("authorListContainerRef");
+const authorListInnerElRef = useTemplateRef("authorListInnerElRef");
+const authorListContainerElWidth = ref(0);
+const authorListInnerElWidth = ref(0);
+
+useResizeObserver(authorListContainerElRef, ([entry]) => {
+  const { width } = entry.contentRect;
+  authorListContainerElWidth.value = width;
+});
+
+useResizeObserver(authorListInnerElRef, ([entry]) => {
+  const { width } = entry.contentRect;
+  authorListInnerElWidth.value = width;
+});
+
+const { play, pause, animate } = useAnimate(
+  authorListInnerElRef,
+  computed(() => {
+    return [
+      {
+        transform: "translate3d(0, 0, 0)",
+        offset: 0,
+      },
+      {
+        transform: "translate3d(0, 0, 0)",
+        offset: 0.3,
+      },
+      {
+        transform: `translate3d(${-authorListInnerElWidth.value + authorListContainerElWidth.value}px, 0, 0)`,
+        offset: 0.5,
+      },
+      {
+        transform: `translate3d(${-authorListInnerElWidth.value + authorListContainerElWidth.value}px, 0, 0)`,
+        offset: 0.8,
+      },
+      {
+        transform: "translate3d(0, 0, 0)",
+        offset: 1,
+      },
+    ];
+  }),
+  {
+    immediate: false,
+    duration: 1000,
+    iterations: Infinity,
+    easing: "linear",
+  },
+);
+
+watch(
+  () => [authorListInnerElWidth.value, authorListContainerElWidth.value],
+  ([innerWidth, containerWidth]) => {
+    if (innerWidth - containerWidth < 0) {
+      pause();
+      return;
+    }
+    const duration = Math.floor(innerWidth / containerWidth) * 10000;
+    animate.value?.effect?.updateTiming({
+      duration,
+    });
+    play();
+  },
+);
 </script>
 
 <template>
@@ -17,18 +81,23 @@ defineProps<{
     <v-card-item>
       <v-card-title>{{ comic.name }}</v-card-title>
       <v-card-subtitle class="wind-cursor-default">
-        <div class="wind-flex wind-flex-wrap wind-gap-2">
-          <!-- TODO route name -->
-          <router-link
-            v-for="author of comic.author"
-            :key="author.path_word"
-            :to="{
-              name: 'COMIC_HOME',
-              query: { query: author.name },
-            }"
+        <div class="wind-max-w-full" ref="authorListContainerRef">
+          <div
+            ref="authorListInnerElRef"
+            class="wind-inline-flex wind-gap-2 wind-truncate"
           >
-            {{ author.name }}
-          </router-link>
+            <!-- TODO route name -->
+            <router-link
+              v-for="author of comic.author"
+              :key="author.path_word"
+              :to="{
+                name: 'COMIC_HOME',
+                query: { query: author.name },
+              }"
+            >
+              {{ author.name }}
+            </router-link>
+          </div>
         </div>
       </v-card-subtitle>
     </v-card-item>
