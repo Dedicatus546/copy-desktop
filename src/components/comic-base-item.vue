fcaa911bbb7505c92b7041c2ed53313638c73574
Par9uet <1607611087@qq.com>
2025-06-07 15:38:04 +0800

    fix: 修复动画 duration 错误

--- src/components/comic-base-item.vue
+++ src/components/comic-base-item.vue
@@ -61,7 +61,10 @@ watch(
       pause();
       return;
     }
-    const duration = Math.floor(innerWidth / containerWidth) * 10000;
+    const duration =
+      containerWidth === 0
+        ? 0
+        : Math.floor(innerWidth / containerWidth) * 10000;
     animate.value?.effect?.updateTiming({
       duration: duration <= 0 ? "auto" : duration,
     });
