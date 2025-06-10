baf2515e0e5a3656dce9aac3928a002e6a77e834
Par9uet <1607611087@qq.com>
2025-06-10 14:50:38 +0800

    refactor: 重构代码

--- src/components/app-light-novel-list-item.vue
+++ src/components/app-light-novel-list-item.vue
@@ -14,73 +14,6 @@ const props = withDefaults(
 const cover = computed(() =>
   import.meta.env.DEV ? "/360x640.svg" : props.lightNovel.cover,
 );
-
-const authorListContainerElRef = useTemplateRef("authorListContainerRef");
-const authorListInnerElRef = useTemplateRef("authorListInnerElRef");
-const authorListContainerElWidth = ref(0);
-const authorListInnerElWidth = ref(0);
-
-useResizeObserver(authorListContainerElRef, ([entry]) => {
-  const { width } = entry.contentRect;
-  authorListContainerElWidth.value = width;
-});
-
-useResizeObserver(authorListInnerElRef, ([entry]) => {
-  const { width } = entry.contentRect;
-  authorListInnerElWidth.value = width;
-});
-
-const { play, pause, animate } = useAnimate(
-  authorListInnerElRef,
-  computed(() => {
-    return [
-      {
-        transform: "translate3d(0, 0, 0)",
-        offset: 0,
-      },
-      {
-        transform: "translate3d(0, 0, 0)",
-        offset: 0.3,
-      },
-      {
-        transform: `translate3d(${-authorListInnerElWidth.value + authorListContainerElWidth.value}px, 0, 0)`,
-        offset: 0.5,
-      },
-      {
-        transform: `translate3d(${-authorListInnerElWidth.value + authorListContainerElWidth.value}px, 0, 0)`,
-        offset: 0.8,
-      },
-      {
-        transform: "translate3d(0, 0, 0)",
-        offset: 1,
-      },
-    ];
-  }),
-  {
-    immediate: false,
-    duration: 1000,
-    iterations: Infinity,
-    easing: "linear",
-  },
-);
-
-watch(
-  () => [authorListInnerElWidth.value, authorListContainerElWidth.value],
-  ([innerWidth, containerWidth]) => {
-    if (innerWidth - containerWidth < 0) {
-      pause();
-      return;
-    }
-    const duration =
-      containerWidth === 0
-        ? 0
-        : Math.floor(innerWidth / containerWidth) * 10000;
-    animate.value?.effect?.updateTiming({
-      duration: duration <= 0 ? "auto" : duration,
-    });
-    play();
-  },
-);
 </script>
 
 <template>
@@ -101,26 +34,21 @@ watch(
       <v-card-item>
         <v-card-title>{{ lightNovel.name }}</v-card-title>
         <v-card-subtitle class="wind-cursor-default">
-          <div class="wind-max-w-full" ref="authorListContainerRef">
-            <div
-              ref="authorListInnerElRef"
-              class="wind-inline-flex wind-gap-2 wind-truncate"
+          <app-scroll-wrapper>
+            <router-link
+              v-for="author of lightNovel.author"
+              :key="author.path_word"
+              :to="{
+                name: 'COMIC_AUTHOR',
+                params: {
+                  authorPathWord: author.path_word,
+                  authorName: author.name,
+                },
+              }"
             >
-              <router-link
-                v-for="author of lightNovel.author"
-                :key="author.path_word"
-                :to="{
-                  name: 'COMIC_AUTHOR',
-                  params: {
-                    authorPathWord: author.path_word,
-                    authorName: author.name,
-                  },
-                }"
-              >
-                {{ author.name }}
-              </router-link>
-            </div>
-          </div>
+              {{ author.name }}
+            </router-link>
+          </app-scroll-wrapper>
         </v-card-subtitle>
       </v-card-item>
     </v-card>
