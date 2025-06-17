ed67bd48f5708ec6a5114eb0e722a971e4b00225
Par9uet <1607611087@qq.com>
2025-06-17 08:55:15 +0800

    refactor: 动漫列表项在公司数据存在的情况下展示

--- src/components/app-anime-list-item.vue
+++ src/components/app-anime-list-item.vue
@@ -7,6 +7,10 @@ const props = withDefaults(
       name: string;
       path_word: string;
       cover: string;
+      company?: {
+        name: string;
+        path_word: string;
+      };
     };
     replace?: boolean;
   }>(),
@@ -35,6 +39,21 @@ const cover = computed(() => resolveCover(props.anime.cover));
       />
       <v-card-item>
         <v-card-title>{{ anime.name }}</v-card-title>
+        <v-card-subtitle v-if="anime.company">
+          <app-scroll-wrapper>
+            <router-link
+              :to="{
+                name: 'ANIME_COMPANY',
+                params: {
+                  companyPathWord: anime.company.path_word,
+                  companyName: anime.company.name,
+                },
+              }"
+            >
+              {{ anime.company.name }}
+            </router-link>
+          </app-scroll-wrapper>
+        </v-card-subtitle>
       </v-card-item>
     </v-card>
   </router-link>
