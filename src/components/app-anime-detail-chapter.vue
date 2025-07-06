966e7b398d23b93be4e5e8395e4ca4181a23258b
Par9uet <1607611087@qq.com>
2025-07-06 22:48:48 +0800

    feat: 动漫详情页支持下载显示进度

--- src/components/app-anime-detail-chapter.vue
+++ src/components/app-anime-detail-chapter.vue
@@ -4,6 +4,7 @@ import { usePagination, useRequest } from "alova/client";
 import { getAnimeChapterDetailApi, getAnimeChapterListApi } from "@/apis";
 import { trpcClient } from "@/apis/ipc";
 import EMPTY_STATE_IMG from "@/assets/empty-state/1.jpg";
+import useDialog from "@/compositions/use-dialog";
 import useSnackbar from "@/compositions/use-snack-bar";
 import { createLogger } from "@/logger";
 import { useDownloadStore } from "@/stores/use-download-store";
@@ -23,6 +24,7 @@ const lastReadChapterModel = defineModel<{
 }>("lastReadChapter");
 
 const snackbar = useSnackbar();
+const dialog = useDialog();
 const downloadStore = useDownloadStore();
 const userStore = useUserStore();
 
@@ -72,6 +74,34 @@ const downloadAnime = async (
     snackbar.warning("未检测到 ffmpeg 命令，请将其添加到 PATH 中");
     return;
   }
+  const key = animePathWord + "-" + animeChapterUuid;
+  if (downloadStore.animeDownloadingMap[key]) {
+    snackbar.warning("任务正在下载中，请勿重复点击");
+    return;
+  }
+  if (downloadStore.animeCompleteMap[key]) {
+    dialog({
+      width: 300,
+      title: "确认",
+      content: "该集已下载，是否重新下载？",
+      async onOk() {
+        await send(animeChapterUuid, linePathWord);
+        downloadStore.addDownloadTaskAction({
+          uuid: await trpcClient.getUuid.query(),
+          type: "anime",
+          animeName,
+          animePathWord,
+          chapterName: animeChapterName,
+          chapterId: animeChapterUuid,
+          videoM3u8Url: chapterData.value.results.chapter.video,
+          filepath: "",
+        });
+        snackbar.success("添加下载任务成功");
+        info("添加 %s %s 下载任务", animeName, animeChapterName);
+      },
+    });
+    return;
+  }
   await send(animeChapterUuid, linePathWord);
   downloadStore.addDownloadTaskAction({
     uuid: await trpcClient.getUuid.query(),
@@ -84,7 +114,7 @@ const downloadAnime = async (
     filepath: "",
   });
   snackbar.success("添加下载任务成功");
-  info("添加 %s 下载任务", animeName);
+  info("添加 %s %s 下载任务", animeName, animeChapterName);
 };
 </script>
 
@@ -109,15 +139,12 @@ const downloadAnime = async (
     </template>
     <template #default="{ items }">
       <v-list>
-        <template v-for="(item, index) of items" :key="item.raw.uuid">
-          <v-divider v-if="index > 0"></v-divider>
-          <v-list-item>
-            <v-list-item-title>{{ item.raw.name }}</v-list-item-title>
-            <template v-slot:append>
-              <template v-if="userStore.isLogin">
+        <v-list-item v-for="item of items" :key="item.raw.uuid">
+          <v-list-item-title>{{ item.raw.name }}</v-list-item-title>
+          <template v-slot:append>
+            <template v-if="userStore.isLogin">
+              <template v-for="line of item.raw.lines" :key="line.path_word">
                 <router-link
-                  v-for="line of item.raw.lines"
-                  :key="line.path_word"
                   :to="{
                     name: 'ANIME_WATCH',
                     params: {
@@ -130,6 +157,7 @@ const downloadAnime = async (
                 >
                   <template #default="{ navigate }">
                     <v-btn
+                      variant="flat"
                       color="primary"
                       class="chapter-btn"
                       @click="
@@ -141,31 +169,61 @@ const downloadAnime = async (
                         navigate())
                       "
                     >
+                      <template #prepend>
+                        <v-icon icon="mdi-play"></v-icon>
+                      </template>
                       <app-scroll-wrapper>
                         {{ line.name }}
                       </app-scroll-wrapper>
                     </v-btn>
-                    <v-btn
-                      class="wind-ml-2"
-                      @click="
-                        downloadAnime(
-                          item.raw.uuid,
-                          item.raw.name,
-                          line.path_word,
-                        )
-                      "
-                    >
-                      下载
-                    </v-btn>
                   </template>
                 </router-link>
-              </template>
-              <template v-else>
-                <v-btn disabled>登陆后才可观看</v-btn>
+                <v-btn
+                  variant="flat"
+                  :color="
+                    downloadStore.animeDownloadingMap[
+                      animePathWord + '-' + item.raw.uuid
+                    ]
+                      ? 'info'
+                      : downloadStore.animeCompleteMap[
+                            animePathWord + '-' + item.raw.uuid
+                          ]
+                        ? 'success'
+                        : undefined
+                  "
+                  class="wind-ml-2"
+                  @click="
+                    downloadAnime(item.raw.uuid, item.raw.name, line.path_word)
+                  "
+                >
+                  <template #prepend>
+                    <v-icon icon="mdi-download"></v-icon>
+                  </template>
+                  {{
+                    downloadStore.animeDownloadingMap[
+                      animePathWord + "-" + item.raw.uuid
+                    ]
+                      ? "正在下载 " +
+                        ((
+                          downloadStore.animeDownloadingMap[
+                            animePathWord + "-" + item.raw.uuid
+                          ]!.percent * 100
+                        ).toFixed(2) +
+                          "%")
+                      : downloadStore.animeCompleteMap[
+                            animePathWord + "-" + item.raw.uuid
+                          ]
+                        ? "已下载"
+                        : "下载"
+                  }}
+                </v-btn>
               </template>
             </template>
-          </v-list-item>
-        </template>
+            <template v-else>
+              <v-btn variant="flat" disabled>登陆后才可观看和下载</v-btn>
+            </template>
+          </template>
+        </v-list-item>
       </v-list>
     </template>
   </v-data-iterator>
